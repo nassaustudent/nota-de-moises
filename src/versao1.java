50c835773f811f3a1b96d1033b68209884401b82
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-26 15:54:56 -0300

    versão primeira
    apenas um teste simples

--- src/versao1.java
+++ src/versao1.java
@@ -27,35 +27,6 @@ public class DivisaoDeNumeros {
 
 		
 
-		//Verifica se o valor do cálculo da divisão é negativo
-
-		if ((dividendo / divisor < 0)) {
-
-			//Imprime o valor 0 caso o resultdo da divisão seja negativo
-
-			System.out.println("Valor encontrado: 0");
-
-		}
-
-		//Verifica se o cálculo é positivo
-
-		else if (dividendo / divisor > 0) {
-
-			//Imprime o resultado do cálculo caso o resultado seja positivo
-
-			System.out.println("Valor calculado: " + dividendo / divisor);
-
-		}
-
-		//Como o resultado não é positivo e nem negativo, podemos supor que o divisor é zero
-
-		else {
-
-			//Imprime o valor -1 pois o divisor é zero
-
-			System.out.println("-1");
-
-		}
 
 	}
 
