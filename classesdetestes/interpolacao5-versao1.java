03ba5e0e5ebc6f235452d779277bc21beaa399be
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-26 15:52:22 -0300

    Update and rename interpolacao5.java to interpolacao5-versao1.java

--- classesdetestes/interpolacao5-versao1.java
+++ classesdetestes/interpolacao5-versao1.java
@@ -17,18 +17,7 @@ private void metodoPolinomial() {
 n = Integer.parseInt(JOptionPane.showInputDialog(null, "Tamanho dos Vetores:"));
 x = new double[n];
 y = new double[n];
-for (int i = 0; i < n; i++) {
-x[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor X" + i + ":"));
-y[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor Y" + i + ":"));
-}
-valorX = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor de X:"));
-n = x.length - 1;
-xx = (double[]) y.clone();
-for (int i = 0; i < n; ++i) {
-for (int j = 0; j < n - i; ++j) {
-xx[j] = (valorX - x[j]) / (x[i + j + 1] - x[j]) * xx[j + 1]
-+ (valorX - x[i + j + 1]) / (x[j] - x[i + j + 1]) * xx[j];
-}
+
 }
 System.out.println("Valor da Interpolação: " + xx[0]);
 }
