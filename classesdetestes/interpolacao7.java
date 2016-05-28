7422f026585303d796dd52efdfd40ce30a2b5ace
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-28 17:01:43 -0300

    alteração básica para testar usuario

--- classesdetestes/interpolacao7.java
+++ classesdetestes/interpolacao7.java
@@ -2,16 +2,8 @@
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-  int tamanho,cont,i,j;
-     printf("Insira o numero de valores conhecidos da funcao que deseja interpolar:\t");
-     scanf("%d",&tamanho);
-     printf("\n"); 
-  float vetfx[tamanho],vetx[tamanho],x,pdtparcial,pdtfinal,fx;
-     printf("Valores de x pre existentes:\n");
-     for(cont=0;cont<tamanho;cont++){
-        printf("Insira o valor de x%d :\t",cont);
-        scanf("%f",&vetx[cont]);
-     }                                
+
+                              
      printf("\n");   
      printf("Valores de f<x> pre existentes:\n");    
      for(cont=0;cont<tamanho;cont++){
