17079e018b831ce03dc45d78fd99ebc25ce49716
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-30 18:44:25 -0300

    Testando versao
    Testinho 3

---
+++ src/classecdeinter.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class DivisaoDeNumeros {
+
+	public static void main(String[] args) {
+		//Criamos um objeto Scanner para capturar o que foi digitado
+		Scanner input = new Scanner(System.in);
+		//Imprime mensagem para a inserção do primeiro valor
+		System.out.println("Insira o valor do dividendo: ");
+		//Guarda o valor digitado pelo usuário na variável dividendo
+		int dividendo = input.nextInt();
+		//Imprime mensagem para a inserção do segundo valor
+		System.out.println("Insira o valor do divisor: ");
+		//Guarda o valor digitado pelo usuário na variável divisor,
+		int divisor = input.nextInt();
+		
+		
+}
