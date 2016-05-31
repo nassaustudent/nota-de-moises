d2a6a0d75bd96b8dde4dbd489377bfa88794ed35
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-31 19:17:12 -0300

    Tentando iniciar um calculo simples
    Implementação simples do tipo de cálculo

---
+++ classesdetestes/novainterpolacao.java
@@ -0,0 +1,31 @@
+Scanner;
+public class Calc {
+	/*Declaração dos métodos*/
+	public int som(int num1, int num2) {
+		return num1 + num2;
+	}
+	public int sub(int num1, int num2){
+		return num1 - num2;
+	}
+	public int div(int num1,int num2){
+		return num1 / num2;
+	}
+	public int mult(int num1, int num2){
+		return num1 - num2;
+	}
+	
+
+
+///////////////////
+// inclusão do menu
+////////////////////////
+
+
+
+		int num1;
+		int num2;
+	      Scanner input = new Scanner(System.in);  
+		System.out.println("-Escolha uma opção-");
+	    System.out.println("1. Soma");  
+	    System.out.println("2. Subtracao");  
+- [ ] 	    System.out.println("3. Multiplicacao");  
