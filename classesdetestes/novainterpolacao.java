80aeca295c86266870b7f580e1eead5ee655c5ce
Nassau Student <nassaustudent@users.noreply.github.com>
2016-05-31 19:28:29 -0300

    Volta dos metodos
    Implementacao

--- classesdetestes/novainterpolacao.java
+++ classesdetestes/novainterpolacao.java
@@ -2,7 +2,18 @@ Scanner;
 public class Calc {
 	/*Declaração dos métodos*/
 
-
+	public int som(int num1, int num2) {
+		return num1 + num2;
+	}
+	public int sub(int num1, int num2){
+		return num1 - num2;
+	}
+	public int div(int num1,int num2){
+		return num1 / num2;
+	}
+	public int mult(int num1, int num2){
+		return num1 - num2;
+	}
 	
 
 
